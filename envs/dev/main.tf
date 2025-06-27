1660ef48189ca11390a46ebe06deacee4af7e96c
Masaru Ogasawara <m.ogasawara@karasuit.com>
2025-06-27 15:54:30 +0900

    ポートフォリオ作成

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -11,3 +11,21 @@ module "bq" {
   source      = "../../modules/bq"
   email       = local.email
 }
+
+# Artifact Registry
+module "artifactregistry" {
+  source      = "../../modules/artifactregistry"
+  project     = local.project
+  environment = local.environment
+  gcp_region  = local.gcp_region
+}
+
+# CLoud Composer
+module "composer" {
+  source      = "../../modules/composer"
+  id          = local.id
+  project     = local.project
+  environment = local.environment
+  gcp_region  = local.gcp_region
+  sa          = local.sa
+}
